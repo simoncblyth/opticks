a24171a662e5299de613e1a78df1733b1514fbaf
Simon Blyth <simoncblyth@gmail.com>
2015-05-09 17:57:04 +0800

    creating an a2b code lookup out of two name2code mappings, assuming a common name

--- numerics/npy/tests/G4StepNPYTest.cc
+++ numerics/npy/tests/G4StepNPYTest.cc
@@ -2,13 +2,25 @@
 #include "NPY.hpp"
 #include "G4StepNPY.hpp"
 
+
+
 int main()
 {
     NPY* npy = NPY::load("cerenkov", "1");
 
     G4StepNPY cs(npy);
-    cs.loadChromaMaterialMap("/tmp/ChromaMaterialMap.json");
-    cs.dumpChromaMaterialMap();
+
+    const char* apath = "/tmp/ChromaMaterialMap.json" ;
+    const char* aprefix = "/dd/Materials/" ;
+
+    cs.createLookup(apath, aprefix, apath, aprefix);
+
+    for(unsigned int a=0; a < 35 ; a++ )
+    {
+        int b = cs.a2b(a);
+        printf("  a => b %u -> %d \n", a, b );
+    }
+
 
 
     return 0 ;
