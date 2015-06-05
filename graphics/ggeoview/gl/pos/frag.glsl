d006cb45755b74adb59f3856995091945591381c
Simon Blyth <simoncblyth@gmail.com>
2015-06-05 13:56:06 +0800

    attempt to make unselected invisible with alpha fails, so scoot unselected photons off to infinity

--- graphics/ggeoview/gl/pos/frag.glsl
+++ graphics/ggeoview/gl/pos/frag.glsl
@@ -1,11 +1,11 @@
 #version 400
 
-in vec3 colour;
+in vec4 colour;
 out vec4 frag_colour;
 
 void main () 
 {
-    frag_colour = vec4 (colour, 1.0);
+    frag_colour = colour ;
 }
 
 
--- graphics/ggeoview/gl/pos/vert.glsl
+++ graphics/ggeoview/gl/pos/vert.glsl
@@ -10,23 +10,26 @@ layout(location = 3) in ivec4 iflg;
 
 // see tail of generate.cu 
 
-out vec3 colour;
+out vec4 colour;
 
 void main () 
 {
-    colour = vec3(0.5,0.5,0.5) ;
+    float w = 1.0 ; 
+    colour = vec4(0.5,0.5,0.5,1.0) ;
 
     // NB the comparison is using 1-based cos_theta signed boundary codes, 0 means miss
     //
-    if(     iflg.x == Selection.x ) colour = vec3(1.0,0.0,0.0) ;
-    else if(iflg.x == Selection.y ) colour = vec3(0.0,1.0,0.0) ;
-    else if(iflg.x == Selection.z ) colour = vec3(0.0,0.0,1.0) ;
-    else if(iflg.x == Selection.w ) colour = vec3(1.0,1.0,1.0) ;
-    else                            colour = vec3(0.5,0.5,0.5) ;
+    if(     iflg.x == Selection.x ) colour = vec4(1.0,1.0,1.0,1.0) ;
+    else if(iflg.x == Selection.y ) colour = vec4(1.0,0.0,0.0,1.0) ;
+    else if(iflg.x == Selection.z ) colour = vec4(0.0,1.0,0.0,1.0) ;
+    else if(iflg.x == Selection.w ) colour = vec4(0.0,0.0,1.0,1.0) ;
+    else                      
+    {
+        colour = vec4(0.0,0.0,0.0,0.0) ; 
+        w = 0.0 ;      // scoot unselected points off to infinity and beyond
+    }
 
-
-    gl_Position = ModelViewProjection * vec4 (vpos, 1.0);
-
+    gl_Position = ModelViewProjection * vec4 (vpos, w );
     gl_PointSize = 1.0;
 
 }
