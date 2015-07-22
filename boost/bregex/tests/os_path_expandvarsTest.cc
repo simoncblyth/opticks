229016f58c0347547054ec4420fe7cd4b63b2348
Tao Lin <lintao@ihep.ac.cn>
2015-07-22 15:13:45 +0800

    test expand HOME.

--- boost/bregex/tests/os_path_expandvarsTest.cc
+++ boost/bregex/tests/os_path_expandvarsTest.cc
@@ -5,5 +5,13 @@ int main(int argc, char** argv)
 {
     std::string o = os_path_expandvars("$ENV_HOME/graphics/ggeoview/cu/photon.h");
     printf("%s\n", o.c_str());
+    std::string home = os_path_expandvars("$HOME/.opticks/GColors.json");
+    printf("%s\n", home.c_str());
+    std::string home1 = os_path_expandvars("$HOME/.opticks");
+    printf("%s\n", home1.c_str());
+    std::string home2 = os_path_expandvars("$HOME/");
+    printf("%s\n", home2.c_str());
+    std::string home3 = os_path_expandvars("$HOME");
+    printf("%s\n", home3.c_str());
     return 0 ; 
 }
