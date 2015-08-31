968f50a806ef2557b5edd4b851ac56ec1a6320a2
Simon Blyth <simoncblyth@gmail.com>
2015-08-31 20:02:58 +0800

    back to glfw minimal with glfwtriangle- as basis for Thrust interop check

---
+++ graphics/glfw/glfwtriangle/glfwtriangle.bash
@@ -0,0 +1,40 @@
+# === func-gen- : graphics/glfw/glfwtriangle/glfwtriangle fgp graphics/glfw/glfwtriangle/glfwtriangle.bash fgn glfwtriangle fgh graphics/glfw/glfwtriangle
+glfwtriangle-src(){      echo graphics/glfw/glfwtriangle/glfwtriangle.bash ; }
+glfwtriangle-source(){   echo ${BASH_SOURCE:-$(env-home)/$(glfwtriangle-src)} ; }
+glfwtriangle-vi(){       vi $(glfwtriangle-source) ; }
+glfwtriangle-env(){      elocal- ; }
+glfwtriangle-usage(){ cat << EOU
+
+
+* http://antongerdelan.net/opengl/hellotriangle.html
+
+
+EOU
+}
+glfwtriangle-dir(){ echo $(env-home)/graphics/glfw/glfwtriangle ; }
+glfwtriangle-cd(){  cd $(glfwtriangle-dir); }
+glfwtriangle-mate(){ mate $(glfwtriangle-dir) ; }
+glfwtriangle-get(){
+   local dir=$(dirname $(glfwtriangle-dir)) &&  mkdir -p $dir && cd $dir
+
+}
+
+
+glfwtriangle-make()
+{
+   glfwtriangle-cd
+
+   glew-
+   glfw-
+
+   local name=glfwtriangle
+   local bin=/tmp/$name
+
+   clang $name.cc -o $bin \
+        -I$(glew-prefix)/include \
+        -I$(glfw-prefix)/include \
+        -L$(glew-prefix)/lib -lglew  \
+        -L$(glfw-prefix)/lib -lglfw.3  \
+        -framework OpenGL
+
+}
---
+++ graphics/glfw/glfwtriangle/glfwtriangle.cc
@@ -0,0 +1,133 @@
+// http://antongerdelan.net/opengl/hellotriangle.html
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+float points[] = {
+   0.0f,  0.5f,  0.0f,
+   0.5f, -0.5f,  0.0f,
+  -0.5f, -0.5f,  0.0f
+};
+
+const char* vertex_shader =
+"#version 400\n"
+"in vec3 vp;"
+"void main () {"
+"  gl_Position = vec4 (vp, 1.0);"
+"}";
+
+const char* fragment_shader =
+"#version 400\n"
+"out vec4 frag_colour;"
+"void main () {"
+"  frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
+"}";
+
+
+void init_geometry(GLuint& vbo)
+{
+    glGenBuffers (1, &vbo);
+    glBindBuffer (GL_ARRAY_BUFFER, vbo);
+    glBufferData (GL_ARRAY_BUFFER, 9 * sizeof (float), points, GL_STATIC_DRAW);
+}
+
+void init_buffer_description(GLuint& vao, GLuint vbo)
+{
+    glGenVertexArrays (1, &vao);
+    glBindVertexArray (vao);
+    glEnableVertexAttribArray (0);
+    glBindBuffer (GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+}
+
+void init_shader(GLuint& shader_program)
+{
+    GLuint vs = glCreateShader (GL_VERTEX_SHADER);
+    glShaderSource (vs, 1, &vertex_shader, NULL);
+    glCompileShader (vs);
+
+    GLuint fs = glCreateShader (GL_FRAGMENT_SHADER);
+    glShaderSource (fs, 1, &fragment_shader, NULL);
+    glCompileShader (fs);
+
+    glAttachShader (shader_program, fs);
+    glAttachShader (shader_program, vs);
+
+    glLinkProgram (shader_program);
+}
+
+int main () {
+  // start GL context and O/S window using the GLFW helper library
+  if (!glfwInit ()) {
+    fprintf (stderr, "ERROR: could not start GLFW3\n");
+    return 1;
+  } 
+
+    // uncomment these lines if on Apple OS X
+#ifdef __APPLE__
+  glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
+
+  GLFWwindow* window = glfwCreateWindow (640, 480, "Hello Triangle", NULL, NULL);
+  if (!window) {
+    fprintf (stderr, "ERROR: could not open window with GLFW3\n");
+    glfwTerminate();
+    return 1;
+  }
+  glfwMakeContextCurrent (window);
+                                  
+  // start GLEW extension handler
+  glewExperimental = GL_TRUE;
+  glewInit ();
+
+  // get version info
+  const GLubyte* renderer = glGetString (GL_RENDERER); // get renderer string
+  const GLubyte* version = glGetString (GL_VERSION); // version as a string
+  printf ("Renderer: %s\n", renderer);
+  printf ("OpenGL version supported %s\n", version);
+
+  // tell GL to only draw onto a pixel if the shape is closer to the viewer
+  glEnable (GL_DEPTH_TEST); // enable depth-testing
+  glDepthFunc (GL_LESS); // depth-testing interprets a smaller value as "closer"
+
+ 
+  GLuint vbo(0);
+  init_geometry(vbo);
+
+  GLuint vao(0);
+  init_buffer_description(vao, vbo);
+
+  GLuint shader_program = glCreateProgram ();
+  init_shader(shader_program);
+
+
+  while (!glfwWindowShouldClose (window)) 
+  {
+      // wipe the drawing surface clear
+      glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      glUseProgram (shader_program);
+
+      glBindVertexArray (vao);
+      // draw points 0-3 from the currently bound VAO with current in-use shader
+      glDrawArrays (GL_TRIANGLES, 0, 3);
+
+
+      // update other events like input handling 
+      glfwPollEvents ();
+      // put the stuff we've been drawing onto the display
+      glfwSwapBuffers (window);
+  }
+
+ 
+  // close GL context and any other GLFW resources
+  glfwTerminate();
+  return 0;
+}
+
+
+
+
