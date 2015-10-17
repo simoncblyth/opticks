dee4eec21ccd93ba39c0953bbb630fed04009f62
Simon Blyth <simoncblyth@gmail.com>
2015-10-17 11:42:32 +0800

    not requiring prior-aabb intersection, instead just intersect with complete part and then require intersection point in zrange, is working

--- graphics/optixrap/cu/hemi-pmt.cu
+++ graphics/optixrap/cu/hemi-pmt.cu
@@ -25,25 +25,24 @@ rtDeclareVariable(float3, shading_normal, attribute shading_normal, );
 
 
 static __device__
-bool intersect_tubs(optix::Ray& r, const float4& q0, const float4& q1, float3& n )
+void intersect_tubs(const float4& zrg, const float4& q0, const float4& q1, const uint4& identity )
 {
   //float3 position = make_float3(q0.x, q0.y, q0.z);
   //float radius = q0.w;
   //float sizeZ =  q1.x ;  
-
-  return false ;  
+  //
 };
 
 
 template<bool use_robust_method>
 static __device__
-bool intersect_sphere(optix::Ray&r, const float4& q0, float3& n)
+void intersect_sphere(const float4& zrg, const float4& q0, const uint4& identity)
 {
   float3 center = make_float3(q0);
   float radius = q0.w;
 
-  float3 O = r.origin - center;
-  float3 D = r.direction;
+  float3 O = ray.origin - center;
+  float3 D = ray.direction;
 
   float b = dot(O, D);
   float c = dot(O, O)-radius*radius;
@@ -57,89 +56,99 @@ bool intersect_sphere(optix::Ray&r, const float4& q0, float3& n)
        radius,
        disc);  
   */
-  
 
-  if(disc > 0.0f)
-  {
-    float sdisc = sqrtf(disc);
-    float root1 = (-b - sdisc);
-    float root11 = 0.0f;
-    bool do_refine = false;
-
-    if(use_robust_method && fabsf(root1) > 10.f * radius) 
+    if(disc > 0.0f)
     {
-        do_refine = true;
-    }
+        float sdisc = sqrtf(disc);
+        float root1 = (-b - sdisc);
+        float root11 = 0.0f;
+        bool do_refine = use_robust_method && fabsf(root1) > 10.f * radius ;  // long ray 
 
-    if(do_refine) // refine root1
-    {
-        float3 O1 = O + root1 * r.direction;
-        b = dot(O1, D);
-        c = dot(O1, O1) - radius*radius;
-        disc = b*b - c;
-
-        if(disc > 0.0f) 
+        if(do_refine) // refine root1
         {
-            sdisc = sqrtf(disc);
-            root11 = (-b - sdisc);
+            float3 O1 = O + root1 * ray.direction;  //  move origin along to 1st intersection point
+            b = dot(O1, D);
+            c = dot(O1, O1) - radius*radius;
+            disc = b*b - c;
+            if(disc > 0.0f) 
+            {
+                sdisc = sqrtf(disc);
+                root11 = (-b - sdisc);
+            }
+        }
+        float3 P = ray.origin + (root1 + root11)*ray.direction ;  
+        if( P.z > zrg.x && P.z < zrg.y )
+        {
+            bool check_second = true;
+            if( rtPotentialIntersection( root1 + root11 ) ) 
+            {
+                shading_normal = geometric_normal = (O + (root1 + root11)*D)/radius;
+                instanceIdentity = identity ; 
+                if(rtReportIntersection(0)) check_second = false;
+            } 
+            if(check_second) 
+            {
+                float root2 = (-b + sdisc) + (do_refine ? root11 : 0.f);   // unconfirmed change root1 -> root11
+                P = ray.origin + root2*ray.direction ;  
+                if( P.z > zrg.x && P.z < zrg.y )
+                { 
+                    if( rtPotentialIntersection( root2 ) ) 
+                    {
+                        shading_normal = geometric_normal = (O + root2*D)/radius;
+                        instanceIdentity = identity ; 
+                        rtReportIntersection(0);   // material index 0 
+                    }
+                }
+            }
         }
     }
-
-    float t0 = root1 + root11 ;
-    float t1 = (-b + sdisc) + (do_refine ? t0 : 0);
-    float tmin = fminf( t0, t1 );
-
-    if( tmin < r.tmin  )
-    {
-        r.tmin = tmin ; 
-        n =  (O + tmin*D)/radius;
-        return true ; 
-    }
-  }
-  return false ;
 }
 
 static __device__
-bool intersect_aabb(optix::Ray &r, const float4& q2, const float4& q3, float3& n)
+void intersect_aabb(const float4& q2, const float4& q3, const uint4& identity)
 {
   const float3 min_ = make_float3(q2.x, q2.y, q2.z); 
   const float3 max_ = make_float3(q3.x, q3.y, q3.z); 
 
-  float3 t0 = (min_ - r.origin)/r.direction;
-  float3 t1 = (max_ - r.origin)/r.direction;
+  float3 t0 = (min_ - ray.origin)/ray.direction;
+  float3 t1 = (max_ - ray.origin)/ray.direction;
+
+  // slab method 
   float3 near = fminf(t0, t1);
   float3 far = fmaxf(t0, t1);
   float tmin = fmaxf( near );
   float tmax = fminf( far );
 
-  if(tmin <= tmax && tmin <= r.tmax) 
+  float3 n = make_float3(0.f);  
+
+  if(tmin <= tmax) 
   {
-      r.tmin = max(r.tmin,tmin);
+      if(rtPotentialIntersection(tmin))
+      {
+          // hmm what about inside box ?
+          if(     tmin == near.x) n.x = 1. ;
+          else if(tmin == near.y) n.y = 1. ;
+          else if(tmin == near.z) n.z = 1. ;
 
-      n.x = 0. ; 
-      n.y = 0. ; 
-      n.z = 0. ; 
-      if(     tmin == near.x) n.x = 1. ;
-      else if(tmin == near.y) n.y = 1. ;
-      else if(tmin == near.z) n.z = 1. ;
-
-      return true;
+          shading_normal = geometric_normal = n ;
+          instanceIdentity = identity ;
+          rtReportIntersection(0);   // material index 0 
+      } 
   }
-  return false;
 }
 
 static __device__
-bool intersect_aabb(optix::Ray &r, const float4& q2, const float4& q3)
+bool intersect_aabb(const float4& q2, const float4& q3)
 {
     const float3 min_ = make_float3(q2.x, q2.y, q2.z); 
     const float3 max_ = make_float3(q3.x, q3.y, q3.z); 
-    float3 t0 = (min_ - r.origin)/r.direction;
-    float3 t1 = (max_ - r.origin)/r.direction;
+    float3 t0 = (min_ - ray.origin)/ray.direction;
+    float3 t1 = (max_ - ray.origin)/ray.direction;
     float3 near = fminf(t0, t1);
     float3 far = fmaxf(t0, t1);
     float tmin = fmaxf( near );
     float tmax = fminf( far );
-    return tmin <= tmax && tmin <= r.tmax ;
+    return tmin <= tmax ;
 }
 
 
@@ -153,9 +162,6 @@ RT_PROGRAM void intersect(int primIdx)
 
   //rtPrintf("intersect primIdx %d numParts %u \n", primIdx, numParts );
 
-  optix::Ray tmp_ray = ray;
-  float3 n ;  
-
   for(unsigned int p=0 ; p < numParts ; p++)
   {  
       unsigned int partIdx = solid.x + p ;  
@@ -165,32 +171,22 @@ RT_PROGRAM void intersect(int primIdx)
       const float4& q2 = partBuffer[4*partIdx+2] ;
       const float4& q3 = partBuffer[4*partIdx+3]; 
 
+      float4 zrange = make_float4( q2.z , q3.z, 0.f, 0.f ) ;
       int typecode = __float_as_int(q2.w); 
-      //rtPrintf("intersect p %u partIdx %u type %d \n", p, partIdx, typecode);
-
-      if(!intersect_aabb(tmp_ray, q2, q3)) continue ; 
 
       switch(typecode)
       {
           case 0:
-             intersect_aabb(tmp_ray, q2, q3, n);
-             break ; 
+                intersect_aabb(q2, q3, identity);
+                break ; 
           case 1:
-             intersect_sphere<true>(tmp_ray, q0, n);
-             break ; 
+                intersect_sphere<true>(zrange, q0, identity);
+                break ; 
           case 2:
-             intersect_tubs(tmp_ray,q0,q1,n);
-             break ; 
+                intersect_tubs(zrange,q0,q1, identity);
+                break ; 
       }
   }
-
-  float t = tmp_ray.tmin ; 
-  if(rtPotentialIntersection(t))
-  {
-      shading_normal = geometric_normal = n ;
-      instanceIdentity = identity ;
-      rtReportIntersection(0);   // material index 0 
-  } 
 
 }
 
