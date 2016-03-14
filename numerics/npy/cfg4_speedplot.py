7cce8a1ea8e20eddc9f3ca1310111f827977026a
Simon Blyth <simoncblyth@gmail.com>
2016-03-14 13:58:03 +0800

    presentation updating for PmtInBox, shift focus to DYB

--- numerics/npy/cfg4_speedplot.py
+++ numerics/npy/cfg4_speedplot.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from env.numerics.npy.metadata import Metadata, Catdir
 
 
-def speedplot(cat, tag, a):
+def speedplot(cat, tag, a, landscape=False, ylim=None, log_=False):
     nnp = len(np.unique(a.numphotons))
     assert nnp == 1, "Tags and negated counterparts should always have the same photon statistics" 
 
@@ -31,7 +31,12 @@ def speedplot(cat, tag, a):
 
     n = len(msks)
     for i, msk in enumerate(msks):
-        ax = fig.add_subplot(n,1,i+1)
+
+        if landscape: 
+            ax = fig.add_subplot(1,n,i+1)
+        else:
+            ax = fig.add_subplot(n,1,i+1)
+        pass
         d = a[msk]
 
         t = d.propagate
@@ -46,12 +51,21 @@ def speedplot(cat, tag, a):
 
         ax.plot( d.index, d.propagate, "o")
         ax.plot( d.index, d.propagate, drawstyle="steps", label=label)
-        ax.set_ylim(ylims[i])
+
+        if log_:
+            ax.set_yscale("log")
+
+        if ylim is not None:
+            ax.set_ylim(ylim)
+        else:
+            ax.set_ylim(ylims[i])
+        pass
         ax.legend(loc=loc)
     pass
 
 
     ax.set_xlabel('All times from: MacBook Pro (2013), NVIDIA GeForce GT 750M 2048 MB (384 cores)')
+    ax.xaxis.set_label_coords(-0.5, -0.07 )
 
     plt.show()
 
@@ -69,6 +83,6 @@ if __name__ == '__main__':
 
 
 if 1:
-    speedplot(cat, tag, a)
+    speedplot(cat, tag, a, landscape=True, ylim=[0.1, 60], log_=True)
     
 
