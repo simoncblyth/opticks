9776e2a0425cc245f8dfcb9b63de73e93138ee72
Simon Blyth <simoncblyth@gmail.com>
2016-04-28 19:29:25 +0800

    cmakecheck- notes

--- cmakecheck/cmakecheck.bash
+++ cmakecheck/cmakecheck.bash
@@ -7,39 +7,7 @@ cmakecheck-usage(){ cat << EOU
 
 ::
 
-    [blyth@ntugrid5 env]$ cmakecheck-configure
-
-
-    CMake Error at /home/blyth/local/env/tools/cmake/cmake-3.5.2-Linux-x86_64/share/cmake-3.5/Modules/FindBoost.cmake:1657 (message):
-      Unable to find the requested Boost libraries.
-
-      Boost version: 1.41.0
-
-      Boost include path: /usr/include
-
-      Could not find the following Boost libraries:
-
-              boost_log
-              boost_log_setup
-
-::
-
-    [blyth@ntugrid5 env]$ cmakecheck-;cmakecheck-configure -DBOOST_ROOT=$(boost-prefix)
-
-    -- Boost version: 1.60.0
-    -- Found the following Boost libraries:
-    --   system
-    --   thread
-    --   program_options
-    --   log
-    --   log_setup
-    --   filesystem
-    --   regex
-    --   chrono
-    --   date_time
-    --   atomic
-
-
+    cmakecheck-;cmakecheck-configure -DDUMP=OFF -DBOOST_ROOT=$(boost-prefix) -D
 
 
 EOU
