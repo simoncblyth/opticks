7c4d380df11291cf917a991db6764e65c351e41d
Tao Lin <lintao@ihep.ac.cn>
2016-07-03 21:31:54 +0800

    by default, the openmesh is not build using debug mode in linux.

--- cmake/Modules/FindOpenMesh.cmake
+++ cmake/Modules/FindOpenMesh.cmake
@@ -9,6 +9,10 @@ if(WIN32)
 else(WIN32)
    set(OpenMesh_SUFFIX "d")
 endif()
+## For SUSE Linux without debug libs.
+if(UNIX AND NOT APPLE)
+   set(OpenMesh_SUFFIX "")
+endif(UNIX AND NOT APPLE)
 
 #  Release libs : OpenMeshCore  OpenMeshTools
 #  Debug libs :   OpenMeshCored OpenMeshToolsd
