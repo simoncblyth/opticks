52f1b3b4daa1bebb2d4f474c909b8679ae2aa26f
Simon Blyth <simoncblyth@gmail.com>
2017-10-18 10:59:26 +0800

    simplify visit_instance by directly getting ray.origin to instance distance in OBJECT frame rather than pointlessly converting into WORLD frame first

--- optixrap/cu/visit_instance.cu
+++ optixrap/cu/visit_instance.cu
@@ -5,42 +5,49 @@
 using namespace optix;
 
 rtDeclareVariable(optix::Ray, ray, rtCurrentRay, );
-//rtDeclareVariable(float4, instance_position, , );
 rtDeclareVariable(float, instance_size, , );
 
 RT_PROGRAM void visit_instance()
 {
+    const float distance = length( ray.origin ) ;  // Visit program ray.origin is in OBJECT frame
+    const unsigned level = distance < instance_size ? 0u : 1u ;  
+    rtIntersectChild( level );
+}
+
+RT_PROGRAM void visit_instance_WORLD()
+{
     /*
+    Transform gymnastics here actually pointless... 
+    No need to convert between frames, OBJECT -> WORLD 
+    just directly use OBJECT frame ray.origin to yield 
+    the same distance.
+
         0  1  2  3
         4  5  6  7
         8  9 10 11
        12 13 14 15 
     */
-
+    
     float matrix[16] ;
- 
     rtGetTransform( RT_OBJECT_TO_WORLD , matrix ) ;
-
     const float3 ipos = make_float3( matrix[3], matrix[7], matrix[11] );  // 12 13 14 was (0,0,0)
-
     const float3 orig = rtTransformPoint( RT_OBJECT_TO_WORLD, ray.origin );
-
     const float3 offset = orig - ipos  ;  
+    const float distance = length( offset ) ; 
+    const float distance1 = length( ray.origin ) ; 
+    const unsigned level = distance < instance_size ? 0u : 1u ;  
 
-    float distance = length( offset ) ; 
-
-    unsigned level = distance < instance_size ? 0u : 1u ;  
-
-    //if(level == 0u)
-    rtPrintf("visit_instance: level %d instance_size %10.3f distance %10.3f  orig (%10.3f %10.3f %10.3f) jpos (%10.3f %10.3f %10.3f)  \n", 
+    rtPrintf("visit_instance_WORLD: level %d instance_size %10.3f distance %10.3f distance1 %10.3f  orig (%10.3f %10.3f %10.3f) ipos (%10.3f %10.3f %10.3f)  \n", 
           level, 
           instance_size,
           distance,
+          distance1,
           orig.x, orig.y, orig.z,
           ipos.x, ipos.y, ipos.z
        ); 
-
+    
     rtIntersectChild( level );
 }
+
 
 
