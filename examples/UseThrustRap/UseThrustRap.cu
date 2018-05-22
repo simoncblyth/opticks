e2439c3560ea2a258d4363ddbb9d34f9c41c4163
Simon Blyth <simoncblyth@gmail.com>
2018-05-22 20:17:04 +0800

    bcm wave reaches UseThrustRap

---
+++ examples/UseThrustRap/UseThrustRap.cu
@@ -0,0 +1,51 @@
+
+#include <iostream> 
+#include <iomanip> 
+
+
+#include <thrust/device_vector.h>
+#include <curand_kernel.h> 
+
+#include "SSys.hh"
+#include "NPY.hpp"
+#include "TRngBuf.hh"
+#include "TUtil.hh"
+
+#include "PLOG.hh"
+
+
+int main(int argc, char** argv)
+{
+    PLOG_(argc, argv);
+
+    LOG(info) << argv[0] ;
+
+    static const unsigned NI = 100000 ; 
+    static const unsigned NJ = 16 ; 
+    static const unsigned NK = 16 ; 
+
+    NPY<double>* ox = NPY<double>::make(NI, NJ, NK);
+    ox->zero();
+
+    thrust::device_vector<double> dox(NI*NJ*NK);
+
+    CBufSpec spec = make_bufspec<double>(dox); 
+
+    TRngBuf<double> trb(NI, NJ*NK, spec );
+
+    trb.generate(); 
+
+    trb.download<double>(ox, true) ; 
+  
+    const char* path = "$TMP/TRngBufTest.npy" ; 
+    //
+    //  import os, numpy as np ; a = np.load(os.path.expandvars("$TMP/TRngBufTest.npy"))
+
+    ox->save(path)  ;
+
+    SSys::npdump(path, "np.float64", NULL, "suppress=True,precision=8" );
+
+    cudaDeviceSynchronize();  
+}
+
+
