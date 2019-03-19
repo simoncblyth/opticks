bb2c8c16ac2c8987153f32f44fbdf64c95557d40
Simon Blyth <simoncblyth@gmail.com>
2019-03-19 16:52:08 +0800

    use python md5 digest in np.py for consistency between macOS and Linux

--- bin/np.py
+++ bin/np.py
@@ -3,11 +3,53 @@
 python -c "import numpy as np, sys ; np.set_printoptions(suppress=True) ; print np.load(sys.argv[1]) " 
 """
 import sys, fnmatch, os, logging, numpy as np, commands
+try: 
+    from hashlib import md5
+except ImportError: 
+    from md5 import md5
+
 log = logging.getLogger(__name__)
 
 np.set_printoptions(suppress=True, precision=4, linewidth=200)
 is_npy_ = lambda _:fnmatch.fnmatch(_,"*.npy")
 is_txt_ = lambda _:fnmatch.fnmatch(_,"*.txt")
+
+
+cumulative = None
+
+def digest_(path):
+    """
+    :param path:
+    :return: md5 hexdigest of the content of the path or None if non-existing path
+
+    http://stackoverflow.com/questions/1131220/get-md5-hash-of-a-files-without-open-it-in-python
+
+    Confirmed to give same hexdigest as commandline /sbin/md5::
+
+        md5 /Users/blyth/workflow/notes/php/property/colliers-4q2011.pdf 
+        MD5 (/Users/blyth/workflow/notes/php/property/colliers-4q2011.pdf) = 3a63b5232ff7cb6fa6a7c241050ceeed
+
+    """
+    global cumulative
+
+    if not os.path.exists(path):return None
+    if os.path.isdir(path):return None
+    dig = md5()
+
+    if cumulative is None:
+        cumulative = md5() 
+
+    with open(path,'rb') as f: 
+        for chunk in iter(lambda: f.read(8192),''): 
+            dig.update(chunk)
+            cumulative.update(chunk)
+        pass
+    return dig.hexdigest()
+
+
+
+
+
 
 def dump_one(a, verbose):
     print(a.shape)
@@ -24,13 +66,17 @@ def dump_tree(base=".", verbose=0):
         for name in filter(is_txt_,files):
             path = os.path.join(root, name)
             lines = len(file(path, "r").readlines())
-            print("%20s : %s " % ( path, lines))
+            print("%40s : %s " % ( path, lines))
         pass
         for name in filter(is_npy_,files):
             path = os.path.join(root, name)
             a = np.load(path)
-            fdig = commands.getoutput("md5 %s" % path).split()[-1]
-            print("%20s : %20s : %s " % ( path, repr(a.shape), fdig))
+            ## 
+            ##fdig = commands.getoutput("md5 %s" % path).split()[-1]   
+            ## md5 on macOS, md5sum on Linux : so use python for consistency
+            ## 
+            fdig = digest_(path)
+            print("%40s : %20s : %s " % ( path, repr(a.shape), fdig ))
             if verbose > 0:
                 dump_one(a, verbose)
             pass
