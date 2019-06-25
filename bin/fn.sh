9b90d152baf55a40f2d63b0dfae2b5d36a39ba3c
Simon Blyth <simoncblyth@gmail.com>
2019-06-25 17:14:22 +0800

    Linux shebang with /usr/bin/env only admits a single argument, so cannot use typical bash -l, hence revert to /bin/bash -l

--- bin/fn.sh
+++ bin/fn.sh
@@ -1,4 +1,27 @@
-#!/usr/bin/env bash -l
+#!/usr/bin/env bash 
+
+notes(){ cat << EON
+
+
+* https://stackoverflow.com/questions/4303128/how-to-use-multiple-arguments-for-awk-with-a-shebang-i-e
+
+Normal approach of using "bash -l" to source the 
+login scripts doesnt work on Linux together with shebang
+(it does work on macOS):: 
+
+     #!/usr/bin/env bash -l
+
+So in the below find OPTICKS_HOME from the path to this script.
+But this is not a practical general solution as users exporting 
+OPTICKS_KEY in .bashrc to pick a geometry is expected usage.
+
+EON
+}
+
+
+export OPTICKS_HOME=$(dirname $(dirname $0))
+opticks-(){ . $OPTICKS_HOME/opticks.bash && opticks-env $* ; } 
+opticks-
 
 fn-
 
--- bin/tboolean.sh
+++ bin/tboolean.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -l 
+#!/bin/bash -l 
 
 tboolean-sh-notes(){ cat << EON
 /**
