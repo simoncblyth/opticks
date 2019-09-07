c4ccaa5075f833d610801e1a3035fef1bc759721
Simon Blyth <simoncblyth@gmail.com>
2019-09-07 14:46:16 +0800

    add Apache2 license headers, using https://github.com/simoncblyth/licensehd driven from env- licensehd- https://bitbucket.org/simoncblyth/env/src/default/tools/licensehd.bash

--- examples/UseOpenMeshRap/UseOpenMeshRap.cc
+++ examples/UseOpenMeshRap/UseOpenMeshRap.cc
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2019 Opticks Team. All Rights Reserved.
+ *
+ * This file is part of Opticks
+ * (see https://bitbucket.org/simoncblyth/opticks).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
 // openmeshrap/tests/DeleteFaceTest.cc
 //  https://www.openmesh.org/media/Documentations/OpenMesh-4.1-Documentation/a00058.html
 //  https://mailman.rwth-aachen.de/pipermail/openmesh/2009-August/000305.html
