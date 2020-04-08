8611d87a940a7268723c310d24eb9ca2d38ac1ad
Simon Blyth <simoncblyth@gmail.com>
2020-04-08 19:28:20 +0100

    example to test the bcm- pkg-config fix when no link libs

---
+++ examples/UseOKConfNoCMake/go.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -l
+##
+## Copyright (c) 2019 Opticks Team. All Rights Reserved.
+##
+## This file is part of Opticks
+## (see https://bitbucket.org/simoncblyth/opticks).
+##
+## Licensed under the Apache License, Version 2.0 (the "License"); 
+## you may not use this file except in compliance with the License.  
+## You may obtain a copy of the License at
+##
+##   http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software 
+## distributed under the License is distributed on an "AS IS" BASIS, 
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
+## See the License for the specific language governing permissions and 
+## limitations under the License.
+##
+
+
+opticks-
+oc-
+
+sdir=$(pwd)
+bdir=/tmp/$USER/opticks/$(basename $sdir)/build 
+
+rm -rf $bdir && mkdir -p $bdir && cd $bdir && pwd 
+
+pkg=OKConf
+
+gcc -c $sdir/Use$pkg.cc $(oc-cflags $pkg)
+gcc Use$pkg.o -o Use$pkg $(oc-libs $pkg) 
+LD_LIBRARY_PATH=$(oc-libpath $pkg) ./Use$pkg
+
+
