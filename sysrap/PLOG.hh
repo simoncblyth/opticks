44a064fb741388a566dd41c80fb5f47824490286
Simon Blyth <simoncblyth@gmail.com>
2020-04-29 15:14:27 +0100

    notes on PLOG logging, particularly the way separate logging control from each lib relies on separate statics visibility hidden

--- sysrap/PLOG.hh
+++ sysrap/PLOG.hh
@@ -33,44 +33,127 @@ using plog::info ;
 using plog::debug ;
 using plog::verbose ;
 
-// hmm dangerous but what alternative 
-//#define trace plog::verbose 
-
 #include "SYSRAP_API_EXPORT.hh"
 
 /**
 
-PLOG : Logging Infrastructure
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+PLOG : Logging Infrastructure 
+--------------------------------
+
+new approach to initialization
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 Opticks executables follow the following pattern.
 
 .. code-block:: c
 
 
-   #include "SYSRAP_LOG.hh"  // headers for each projects logger
-   #include "BRAP_LOG.hh"
-   #include "NPY_LOG.hh"
-   #include "OKCORE_LOG.hh"
-
-   #include "PLOG.hh"        // infrastructure header
+   #include "OPTICKS_LOG.hh"  // brings in separate headers for each projects logger depending on defines such as OPTICKS_GGEO
 
    int main(int argc, char** argv)
    {
-       PLOG_(argc, argv);  // pass arguments to PLOG_ macro
-
-       SYSRAP_LOG__ ;     // setup loggers for all projects you want to see output from
-       BRAP_LOG__ ; 
-       NPY_LOG__ ;       
-       OKCORE_LOG__ ;       
+       OPTICKS_LOG(argc, argv);  // pass arguments to PLOG_ macro
 
        Opticks ok(argc, argv);
 
        //  ...  exercise Opticks  ... 
-
        return 0 ; 
    }
 
+
+
+OPTICKS_LOG is a macro from OPTICKS_LOG.hh that must be placed in the main program::
+
+    #define OPTICKS_LOG(argc, argv) {      PLOG_COLOR(argc, argv);     OPTICKS_LOG_::Initialize(PLOG::instance, plog::get(), NULL ); } 
+
+The PLOG_COLOR macro from PLOG_INIT.hh creates two static appenders in the main compilation unit::
+
+    #define PLOG_COLOR(argc, argv) \
+    { \
+        PLOG* _plog = new PLOG(argc, argv); \
+        static plog::RollingFileAppender<plog::TxtFormatter> fileAppender( _plog->filename, _plog->maxFileSize, _plog->maxFiles ); \
+        static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender; \
+        PLOG_INIT( _plog->level, &consoleAppender, &fileAppender ); \
+    } \
+
+The PLOG_INIT macro also from PLOG_INIT.hh applies plog::init to the main consoleAppender and adds the fileAppender:: 
+
+    #define PLOG_INIT(level, app1, app2 ) \
+    { \
+        plog::IAppender* appender1 = static_cast<plog::IAppender*>(app1) ; \
+        plog::IAppender* appender2 = static_cast<plog::IAppender*>(app2) ; \
+        plog::Severity severity = static_cast<plog::Severity>(level) ; \
+        plog::init( severity ,  appender1 ); \
+        if(appender2) \
+            plog::get()->addAppender(appender2) ; \
+    } \
+
+
+The subsequent OPTICKS_LOG_::Initialize passes the primary instance returned by plog::get() in the 
+main compilation unit to the static logging setup in each of the shared object libraries selected
+by compilation defines such as OPTICKS_SYSRAP, OPTICKS_BRAP. 
+
+Extracts from OPTICKS_LOG.hh showing the OPTICKS_LOG_::Initialize::
+
+    ...
+    #ifdef OPTICKS_SYSRAP
+    #include "SYSRAP_LOG.hh"
+    #endif
+    #ifdef OPTICKS_BRAP
+    #include "BRAP_LOG.hh"
+    #endif
+    #ifdef OPTICKS_NPY
+    #include "NPY_LOG.hh"
+    #endif
+
+    ...
+ 
+    class SYSRAP_API OPTICKS_LOG_ {
+       public:
+           // initialize all linked loggers and hookup the main logger
+           static void Initialize(PLOG* instance, void* app1, void* app2 )
+           {
+               int max_level = instance->parse("info") ;
+               // note : can decrease verbosity from the max_level in the subproj, but not increase
+    
+    #ifdef OPTICKS_SYSRAP
+        SYSRAP_LOG::Initialize(instance->prefixlevel_parse( max_level, "SYSRAP"), app1, NULL );
+    #endif
+    #ifdef OPTICKS_BRAP
+        BRAP_LOG::Initialize(instance->prefixlevel_parse( max_level, "BRAP"), app1, NULL );
+    #endif
+    #ifdef OPTICKS_NPY
+        NPY_LOG::Initialize(instance->prefixlevel_parse( max_level, "NPY"), app1, NULL );
+    #endif
+
+
+From SYSRAP_LOG.cc::
+
+    void SYSRAP_LOG::Initialize(int level, void* app1, void* app2 )
+    {
+        PLOG_INIT(level, app1, app2);
+    }
+
+
+The somewhat bizarre usage implementation based on preprocessor
+macros allows the static logger symbols to be planted within 
+each of the shared objects in a manner that works on Mac, Linux 
+and Windows.  
+
+The structure was based on the Chained example from my fork of the upstream plog
+
+* https://github.com/simoncblyth/plog/tree/master/samples/Chained
+
+The structure relies on the static plog instances in all of the libraries and 
+those in the main being distinct which means that it needs 
+compilation options::
+
+    -fvisibility=hidden
+    -fvisibility-inlines-hidden
+
+These are configured in cmake/Modules/OpticksCXXFlags.cmake
+The big advantage of this is that the logging level can be 
+individually controlled for each of the libraries.
 
 *PLOG* parses command line arguments and configures the 
 logging level of each project, for example:
@@ -114,10 +197,44 @@ The tags for each project are listed below.
 =======================  ============================
 
 
-The somewhat bizarre usage implementation based on preprocessor
-macros allows the static logger symbols to be planted within 
-each of the shared objects in a manner that works on Mac, Linux 
-and Windows.
+
+
+
+
+
+older low level approach
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This old approach has been replaced by the OPTICKS_LOG 
+approach described above, although some test executables
+have yet to be updated. 
+
+.. code-block:: c
+
+
+   #include "SYSRAP_LOG.hh"  // headers for each projects logger
+   #include "BRAP_LOG.hh"
+   #include "NPY_LOG.hh"
+   #include "OKCORE_LOG.hh"
+
+   #include "PLOG.hh"        // infrastructure header
+
+   int main(int argc, char** argv)
+   {
+       PLOG_(argc, argv);  // pass arguments to PLOG_ macro
+
+       SYSRAP_LOG__ ;     // setup loggers for all projects you want to see output from
+       BRAP_LOG__ ; 
+       NPY_LOG__ ;       
+       OKCORE_LOG__ ;       
+
+       Opticks ok(argc, argv);
+
+       //  ...  exercise Opticks  ... 
+
+       return 0 ; 
+   }
+
 
 
 **/
