d8fa102e174bb4a9a5a0a92bd14f2760b3c6ce98
Simon C Blyth <simoncblyth@gmail.com>
2022-05-01 15:55:49 +0100

    Linux needs cstring

--- sysrap/SCVD.hh
+++ sysrap/SCVD.hh
@@ -24,6 +24,7 @@ TODO: perhaps incorporate into OPTICKS_LOG ?
 **/
 
 #include <cstdlib>
+#include <cstring>
 #include <string>
 #include <iostream>
 #include <sstream>
--- sysrap/SEventConfig.cc
+++ sysrap/SEventConfig.cc
@@ -1,4 +1,5 @@
 #include <sstream>
+#include <cstring>
 #include <cassert>
 
 #include "SSys.hh"
