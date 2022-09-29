70e3cf8656faf0c8291449f9621dac428ad607da
Simon C Blyth <simoncblyth@gmail.com>
2022-09-29 17:18:23 +0100

    access the genstep labels to identify gentype of origin photons of hits

--- u4/tests/U4Debug.py
+++ u4/tests/U4Debug.py
@@ -9,31 +9,40 @@ if __name__ == '__main__':
 
     zero = False
     three = True
+
+    # TODO: avoid repetition with U4Debug class
     
     if zero:
         f00 = Fold.Load(base, "ntds0/000", symbol="f00" ) 
-        f01 = Fold.Load(base, "ntds0/001", symbol="f01" ) 
         f00c = f00.U4Cerenkov_Debug.reshape(-1,8) 
-        f01c = f01.U4Cerenkov_Debug.reshape(-1,8) 
         f00s = f00.U4Scintillation_Debug.reshape(-1,8) 
-        f01s = f01.U4Scintillation_Debug.reshape(-1,8) 
         f00h = f00.U4Hit_Debug.reshape(-1,4) 
+        f00g = f00.gsl.reshape(-1,4) 
+        print("f00c %10s f00s %10s f00h %10s f00g %10s " % (str(f00c.shape), str(f00s.shape), str(f00h.shape), str(f00g.shape)) )
+
+        f01 = Fold.Load(base, "ntds0/001", symbol="f01" ) 
+        f01c = f01.U4Cerenkov_Debug.reshape(-1,8) 
+        f01s = f01.U4Scintillation_Debug.reshape(-1,8) 
         f01h = f01.U4Hit_Debug.reshape(-1,4) 
-        print("f00c %10s f00s %10s f00h %10s " % (str(f00c.shape), str(f00s.shape), str(f00h.shape)) )
-        print("f01c %10s f01s %10s f01h %10s " % (str(f01c.shape), str(f01s.shape), str(f01h.shape)) )
+        f01g = f01.gsl.reshape(-1,4) 
+        print("f01c %10s f01s %10s f01h %10s f01g %10s " % (str(f01c.shape), str(f01s.shape), str(f01h.shape), str(f01g.shape)) )
     pass
 
     if three:
         f30 = Fold.Load(base, "ntds3/000", symbol="f30" ) 
-        f31 = Fold.Load(base, "ntds3/001", symbol="f31" ) 
         f30c = f30.U4Cerenkov_Debug.reshape(-1,8) 
-        f31c = f31.U4Cerenkov_Debug.reshape(-1,8) 
         f30s = f30.U4Scintillation_Debug.reshape(-1,8) 
-        f31s = f31.U4Scintillation_Debug.reshape(-1,8) 
         f30h = f30.U4Hit_Debug.reshape(-1,4) 
+        f30g = f30.gsl.reshape(-1,4) 
+        print("f30c %10s f30s %10s f30h %10s f30g %10s " % (str(f30c.shape), str(f30s.shape), str(f30h.shape), str(f30g.shape)) )
+
+        f31 = Fold.Load(base, "ntds3/001", symbol="f31" ) 
+        f31c = f31.U4Cerenkov_Debug.reshape(-1,8) 
+        f31s = f31.U4Scintillation_Debug.reshape(-1,8) 
         f31h = f31.U4Hit_Debug.reshape(-1,4) 
-        print("f30c %10s f30s %10s f30h %10s " % (str(f30c.shape), str(f30s.shape), str(f30h.shape)) )
-        print("f31c %10s f31s %10s f31h %10s " % (str(f31c.shape), str(f31s.shape), str(f31h.shape)) )
+        f31g = f31.gsl.reshape(-1,4) 
+
+        print("f31c %10s f31s %10s f31h %10s f31g %10s " % (str(f31c.shape), str(f31s.shape), str(f31h.shape), str(f31g.shape)) )
     pass
      
     if zero and three:
