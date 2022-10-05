49a0e446269b42c71a436f3a8b09800b905635f3
Simon C Blyth <simoncblyth@gmail.com>
2022-10-05 11:04:50 +0100

    U4Debug class to simplify comparisons, realize that opticksMode 1 steps should not be matching opticksMode 0 as is found, it is modes 0 and 3 that should have exact step match : they do currently

--- u4/tests/U4Debug.py
+++ u4/tests/U4Debug.py
@@ -1,60 +1,89 @@
 #!/usr/bin/env python
+"""
+U4Debug.py
+============
 
-import numpy as np
+Comparing C and S steps between opticksMode
+
+opticksMode:0
+    as if there is no Opticks there
+
+opticksMode:1
+    only GPU propagation, CPU photon generation is skipped
+    (NB because this changes the Geant4 random consumption the C+S steps will
+    not match those obtained wih opticksMode 0)
+
+opticksMode:3
+    both CPU and GPU propagations, the CPU ones should be the same as opticksMode:0
+    (NB because the Geant4 random consumption of the C+S steps is unchanged
+    there should be an exact match between opticksMode 0 and 3)
+
+
+"""
+import os, numpy as np
 from opticks.ana.fold import Fold
 
-if __name__ == '__main__':
+class U4Debug(object):
+    @classmethod
+    def Create(cls, rel, base, symbol):
+        f = Fold.Load(base, rel, symbol=symbol)
+        return None if f is None else cls(f, rel=rel)        
 
+    def __init__(self, f, rel):
+        self.rel = rel
+        self.f = f 
+
+        c = f.U4Cerenkov_Debug.reshape(-1,8) 
+        s = f.U4Scintillation_Debug.reshape(-1,8) 
+        h = f.U4Hit_Debug.reshape(-1,4) 
+        g = f.gsl.reshape(-1,4) if hasattr(getattr(f,'gsl', None),'shape') else None
+
+        self.c = c 
+        self.s = s 
+        self.h = h 
+        self.g = g
+
+    def __str__(self):
+        f = self.f
+        return repr(f)
+
+    def __repr__(self):
+        rel = self.rel
+        c = self.c 
+        s = self.s 
+        h = self.h 
+        g = self.g 
+        gs = str(g.shape) if not g is None else "-"
+        return "%20s c %10s s %10s h %10s g %10s " % (rel, str(c.shape), str(s.shape), str(h.shape), gs) 
+ 
+
+if __name__ == '__main__':
     base = "/tmp/u4debug"
 
-    zero = False
-    three = True
+    x00 = U4Debug.Create("ntds0/000", symbol="x00", base=base)
+    x01 = U4Debug.Create("ntds0/001", symbol="x01", base=base)
 
-    # TODO: avoid repetition with U4Debug class
-    
-    if zero:
-        f00 = Fold.Load(base, "ntds0/000", symbol="f00" ) 
-        f00c = f00.U4Cerenkov_Debug.reshape(-1,8) 
-        f00s = f00.U4Scintillation_Debug.reshape(-1,8) 
-        f00h = f00.U4Hit_Debug.reshape(-1,4) 
-        f00g = f00.gsl.reshape(-1,4) 
-        print("f00c %10s f00s %10s f00h %10s f00g %10s " % (str(f00c.shape), str(f00s.shape), str(f00h.shape), str(f00g.shape)) )
-
-        f01 = Fold.Load(base, "ntds0/001", symbol="f01" ) 
-        f01c = f01.U4Cerenkov_Debug.reshape(-1,8) 
-        f01s = f01.U4Scintillation_Debug.reshape(-1,8) 
-        f01h = f01.U4Hit_Debug.reshape(-1,4) 
-        f01g = f01.gsl.reshape(-1,4) 
-        print("f01c %10s f01s %10s f01h %10s f01g %10s " % (str(f01c.shape), str(f01s.shape), str(f01h.shape), str(f01g.shape)) )
-    pass
-
-    if three:
-        f30 = Fold.Load(base, "ntds3/000", symbol="f30" ) 
-        f30c = f30.U4Cerenkov_Debug.reshape(-1,8) 
-        f30s = f30.U4Scintillation_Debug.reshape(-1,8) 
-        f30h = f30.U4Hit_Debug.reshape(-1,4) 
-        f30g = f30.gsl.reshape(-1,4) 
-        print("f30c %10s f30s %10s f30h %10s f30g %10s " % (str(f30c.shape), str(f30s.shape), str(f30h.shape), str(f30g.shape)) )
-
-        f31 = Fold.Load(base, "ntds3/001", symbol="f31" ) 
-        f31c = f31.U4Cerenkov_Debug.reshape(-1,8) 
-        f31s = f31.U4Scintillation_Debug.reshape(-1,8) 
-        f31h = f31.U4Hit_Debug.reshape(-1,4) 
-        f31g = f31.gsl.reshape(-1,4) 
-
-        print("f31c %10s f31s %10s f31h %10s f31g %10s " % (str(f31c.shape), str(f31s.shape), str(f31h.shape), str(f31g.shape)) )
-    pass
+    x10 = U4Debug.Create("ntds1/000", symbol="x10", base=base)
+    x11 = U4Debug.Create("ntds1/001", symbol="x11", base=base)
+ 
+    x30 = U4Debug.Create("ntds3/000", symbol="x30", base=base)
+    x31 = U4Debug.Create("ntds3/001", symbol="x31", base=base)
      
-    if zero and three:
-        # opticksMode 0<->3 should match : currently relying on Cerenkov pinning 
-        assert np.all( f00c == f30c )
-        assert np.all( f01c == f31c )
-        assert np.all( f00s == f30s )
-        assert np.all( f01s == f31s )
-        assert np.all( f00h == f30h )
-        assert np.all( f01h == f31h )
+    if not x00 is None and not x30 is None:
+        assert np.all( x00.c == x30.c )
+        assert np.all( x00.s == x30.s )
+    else:
+        print("one of x00 or x30 is None")
     pass
-
-
+    if not x01 is None and not x31 is None:
+        assert np.all( x01.c == x31.c )
+        assert np.all( x01.s == x31.s )
+    else:
+        print("one of x01 or x31 is None")
+    pass
+    ## hit records dont match as gensteps are not collected for opticksMode 0 changing hit indices
+    #assert np.all( x00.h == x30.h )  
+    #assert np.all( x01.h == x31.h )
+    pass
 
 
