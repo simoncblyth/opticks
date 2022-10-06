ffb7087ccc656c3c93d74deb0aa9758780af6438
Simon C Blyth <simoncblyth@gmail.com>
2022-10-06 20:16:04 +0100

    update QRng::DEFAULT_PATH to use the new QCurandState files

--- ana/rng.py
+++ ana/rng.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 """
+
+
+  1.00M :    1000000 : QCurandState_1000000_0_0.bin 
+  3.00M :    3000000 : QCurandState_3000000_0_0.bin 
+
+
+
 cuRANDWrapper_100000000_0_0.bin
 cuRANDWrapper_10000000_0_0.bin
 cuRANDWrapper_1000000_0_0.bin
@@ -11,7 +18,11 @@ cuRANDWrapper_3000000_0_0.bin
 
 import os, logging, re
 log = logging.getLogger(__name__)
-ptn = re.compile("^cuRANDWrapper_(?P<num>\d*)_(?P<a>\d*)_(?P<b>\d*).bin$")
+
+#pfx = "cuRANDWrapper"
+pfx = "QCurandState"
+_ptn = "^%s_(?P<num>\d*)_(?P<a>\d*)_(?P<b>\d*).bin$" % pfx
+ptn = re.compile(_ptn)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
@@ -22,6 +33,7 @@ if __name__ == '__main__':
     lines = os.listdir(rngdir)  
     
     for line in lines:
+        if not line.startswith(pfx): continue 
         m = ptn.match(line)
         if not m:
             log.info("failed to match %s " % line)
