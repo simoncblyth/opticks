cece22f82378db0e1859fec1658bea25176fe2ee
Simon C Blyth <simoncblyth@gmail.com>
2022-11-01 20:59:27 +0000

    split off QPMTProp.hh

--- qudarap/tests/QPMTPropTest.cc
+++ qudarap/tests/QPMTPropTest.cc
@@ -1,45 +1,20 @@
+/**
+QPMTPropTest.cc
+=================
+
+QProp::lookup_scan is testing on GPU interpolation
+BUT: the kernel is rather hidden away, need a more open test
+to workout how to integrate with j/Layr/Layr.h TMM calcs
+
+qprop.h is very simple, might as well extend that a little 
+into a qpmtprop.h
+
+**/
+
 #include <cuda_runtime.h>
-#include "QProp.hh"
 #include "JPMTProp.h"
+#include "QPMTProp.hh"
 #include "OPTICKS_LOG.hh"
-
-template<typename T>
-struct QPMTProp
-{
-    const QProp<T>* rindex ; 
-    const NP* thickness ;  
-
-    QPMTProp( const NP* rindex, const NP* thickness );     
-
-    std::string desc() const ; 
-};
-
-template<typename T>
-QPMTProp<T>::QPMTProp( const NP* rindex_ , const NP* thickness_ )
-    :
-    rindex(QProp<T>::Make3D(rindex_)),
-    thickness(thickness_)
-{
-}
-
-template<typename T>
-std::string QPMTProp<T>::desc() const 
-{
-    std::stringstream ss ; 
-    ss << "QPMTProp::desc"
-       << std::endl
-       << "rindex"
-       << std::endl
-       << rindex->desc()
-       << std::endl
-       << "thickness"
-       << std::endl
-       << thickness->desc()
-       << std::endl
-       ;
-    std::string s = ss.str(); 
-    return s ;
-} 
 
 const char* FOLD = "/tmp/QPMTPropTest" ;
 
