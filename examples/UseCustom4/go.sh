749eeff9ea5aa8b32261a1aed2121be9892e35d5
Simon C Blyth <simoncblyth@gmail.com>
2023-07-02 16:10:01 +0100

    remove use of Opticks bash/CMake machinery from examples/UseCustom4/go.sh as Custom4 is upstream from Opticks and should work off the CMAKE_PREFIX_PATH without any need for Opticks

--- examples/UseCustom4/go.sh
+++ examples/UseCustom4/go.sh
@@ -13,26 +13,26 @@ To remove all installed Custom4 libs and headers::
 EOU
 }
 
-opticks-
-oe-
-om-
-
 echo $CMAKE_PREFIX_PATH | tr ":" "\n"
 
 sdir=$(pwd)
 name=$(basename $sdir)
 bdir=/tmp/$USER/opticks/$name/build 
+idir=/tmp/$USER/opticks/$name/install
 
+rm -rf $idir && mkdir -p $idir && cd $idir && pwd 
 rm -rf $bdir && mkdir -p $bdir && cd $bdir && pwd 
 
-om-cmake $sdir 
+cmake $sdir -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$idir 
 make
 make install   
 
-echo $BASH_SOURCE : running executable $name 
+bin=$idir/lib/$name
 
-$name
+echo $BASH_SOURCE : bin $bin : running 
+
+$bin
 rc=$?
 
-echo $BASH_SOURCE : $name executable rc $rc 
+echo $BASH_SOURCE : bin $bin : rc $rc 
 
