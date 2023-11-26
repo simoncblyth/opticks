67ad76535994812b29b0e6cd39e61111aa92cd97
Simon C Blyth <simoncblyth@gmail.com>
2023-11-26 16:06:29 +0800

    start sprof_fold_test

---
+++ sysrap/sprof_fold.h
@@ -0,0 +1,57 @@
+#pragma once
+/**
+sprof_fold.h
+===============
+
+Presents sprof.h profile metadata from a collection of SEvt NPFold
+
+**/
+
+#include "NPFold.h"
+
+struct sprof_fold
+{
+    static constexpr const char* STAMP_KEY = "subprofile" ; 
+    static constexpr const char* DELTA_KEY = "delta_subprofile" ; 
+    static constexpr const char* LABELS_KEY = "labels" ; 
+
+    const NPFold* af ; 
+    const char* symbol ; 
+
+    const NP* st ;  
+    const NP* dt ;
+    const NP* la ;
+    const std::vector<std::string>* rows ; 
+    const std::vector<std::string>* cols ; 
+ 
+    sprof_fold( const NPFold* af, const char* symbol ); 
+    std::string desc() const ; 
+};
+
+
+inline sprof_fold::sprof_fold( const NPFold* af_, const char* symbol_ ) 
+    :
+    af(af_),
+    symbol(symbol_ ? strdup(symbol_) : nullptr),
+    st(af ? af->get(STAMP_KEY) : nullptr),
+    dt(af ? af->get(DELTA_KEY) : nullptr),
+    la(af ? af->get(LABELS_KEY) : nullptr),
+    rows(st ? &(st->names) : nullptr), 
+    cols(la ? &(la->names) : nullptr)
+{
+}
+
+inline std::string sprof_fold::desc() const 
+{
+    std::stringstream ss ; 
+    ss 
+       << "[sprof_fold::desc " << ( symbol ? symbol : "-" )  << std::endl 
+       << ( dt ? dt->descTable<int64_t>(8,cols,rows) : "-" ) << std::endl 
+       << "]sprof_fold::desc " << ( symbol ? symbol : "-" )  << std::endl 
+       ;
+    std::string str =  ss.str() ; 
+    return str ; 
+} 
+    
+
+
---
+++ sysrap/tests/sprof_fold_test.cc
@@ -0,0 +1,115 @@
+/**
+sprof_fold_test.cc : Summarize + Present SEvt/NPFold metadata profile stamps 
+==============================================================================
+
+Summarizes SEvt/NPFold metadata profile time/VM/RS stamps into substamp arrays 
+grouped by NPFold path prefix. The summary NPFold is presented textually 
+and saved to allow plotting from python. 
+
+The machinery is similar to sstampfold_test.cc however some differences:
+
+1. have extra VM and RSS memory values in addition to time stamps
+2. with profile info are more interested in changes between 
+   events (eg memory leaks) and are not concerned with changes 
+   within events which are more relevant to sstampfold_test
+
+TODO
+----- 
+
+* implement C++ textual presentation of the summary info :
+  can start the same as sstampfold but in triplicate
+
+* bring over the plotting machinery from ~/np/tests/NPFold_profile_test.py 
+
+
+
++-----+---------------------------------+-------------------------+
+| key | SEvt/NPFold path prefix         |  SEvt type              |
++=====+=================================+=========================+
+| a   | "//p" eg: //p001 //p002         | Opticks/QSim SEvt       |
++-----+---------------------------------+-------------------------+
+| b   | "//n" eg: //n001 //n002         | Geant4/U4Recorder SEvt  |
++-----+---------------------------------+-------------------------+
+
+The tables are presented with row and column labels and the 
+summary NPFold is saved to ./sprof_fold_test relative to 
+the invoking directory which needs to contain SEvt/NPFold folders 
+corresponding to the path prefix.  
+
+The use of NPFold::LoadNoData means that only SEvt NPFold/NP 
+metadata is loaded. Excluding the array data makes the load 
+very fast and able to handle large numbers of persisted SEvt NPFold. 
+
+Usage::
+
+    epsilon:~ blyth$ cd /data/blyth/opticks/GEOM/J23_1_0_rc3_ok0/jok-tds/ALL0
+
+    epsilon:ALL0 blyth$ find . -name NPFold_meta.txt | head -5
+    ./p009/NPFold_meta.txt
+    ./p007/NPFold_meta.txt
+    ./p001/NPFold_meta.txt
+    ./p006/NPFold_meta.txt
+    ./p008/NPFold_meta.txt
+
+    epsilon:ALL0 blyth$ sprof_fold_test 
+    ...
+    
+    epsilon:ALL0 blyth$ ls -alst ../sprof_fold_test/   ##  ls output NPFold directory 
+    total 8
+    8 -rw-r--r--  1 blyth  staff    4 Nov 26 14:12 NPFold_index.txt
+    0 drwxr-xr-x  9 blyth  staff  288 Nov 26 13:01 b
+    0 drwxr-xr-x  5 blyth  staff  160 Nov 26 13:01 .
+    0 drwxr-xr-x  9 blyth  staff  288 Nov 26 13:01 a
+    0 drwxr-xr-x  5 blyth  staff  160 Nov 26 13:01 ..
+    epsilon:ALL0 blyth$ 
+
+
+**/
+
+#include <cstdlib>
+#include "sprof_fold.h"
+
+
+int main(int argc, char** argv)
+{
+    const char* dirp = argc > 1 ? argv[1] : U::PWD() ;   
+    if(dirp == nullptr) return 0 ; 
+    U::SetEnvDefaultExecutableSiblingPath("FOLD", argv[0], dirp );
+
+    bool VERBOSE = getenv("VERBOSE") != nullptr ; 
+ 
+    NPFold* f = NPFold::LoadNoData(dirp); 
+
+    std::cout 
+        << "sprof_fold_test"
+        << std::endl
+        << "NPFold::LoadNoData(\"" << dirp << "\")" 
+        << std::endl
+        ;
+
+    if(VERBOSE || true) std::cout 
+        << "[sprof_fold_test.VERBOSE "
+        << std::endl
+        << f->desc()
+        << std::endl
+        << "]sprof_fold_test.VERBOSE "
+        << std::endl
+        ; 
+
+    NPFold* smry = f->subfold_summary(sprof_fold::STAMP_KEY, "a://p", "b://n"); 
+    std::cout << smry->desc() << std::endl ;
+
+    const NPFold* a = smry->find_subfold("a"); 
+    const NPFold* b = smry->find_subfold("b"); 
+
+    sprof_fold apr(a, "apr"); 
+    sprof_fold bpr(b, "bpr"); 
+
+    std::cout << apr.desc() ; 
+    std::cout << bpr.desc() ; 
+
+    smry->save_verbose("$FOLD"); 
+
+    return 0 ; 
+}
+
