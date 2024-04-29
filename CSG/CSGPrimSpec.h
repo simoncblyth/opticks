1ece886cbb81d56c7b7db6af1d963e9364274b19
Simon C Blyth <simoncblyth@gmail.com>
2024-04-29 15:52:57 +0800

    check headeronly CSG/CSGPrimSpec.h ahead of migration to sysrap/SCSGPrimSpec.h

--- CSG/CSGPrimSpec.h
+++ CSG/CSGPrimSpec.h
@@ -4,7 +4,6 @@
 #else
 #include <vector>
 #include <string>
-#include "CSG_API_EXPORT.hh"
 #endif
 
 /**
@@ -23,8 +22,11 @@ from 0 to numPrim-1 for that GAS.
 
 **/
 
-struct CSG_API CSGPrimSpec
+struct CSGPrimSpec
 {
+    static constexpr const unsigned CSGPrim__value_offsetof_sbtIndexOffset = 4 ;
+    static constexpr const unsigned CSGPrim__value_offsetof_AABB = 8 ; 
+  
     const float*    aabb ; 
     const unsigned* sbtIndexOffset ;   
     unsigned        num_prim ; 
@@ -41,4 +43,155 @@ struct CSG_API CSGPrimSpec
     std::string desc() const ; 
 #endif
 };
+
+
+
+#if defined(__CUDACC__) || defined(__CUDABE__)
+#else
+
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+#include <cstring>
+
+#include "scuda.h"
+#include "SCU.h"
+
+
+/**
+CSGPrimSpec::gather
+---------------------
+
+Writes num_prim*6 bbox floats into out. 
+
+**/
+
+inline void CSGPrimSpec::gather(std::vector<float>& out) const 
+{
+    //assert( device == false ); 
+    unsigned size_in_floats = 6 ; 
+    out.resize( num_prim*size_in_floats ); 
+
+    unsigned stride_in_floats = stride_in_bytes/sizeof(float) ; 
+    for(unsigned i=0 ; i < num_prim ; i++) 
+    {   
+        float* dst = out.data() + size_in_floats*i ;   
+        const float* src = aabb + stride_in_floats*i ;   
+        memcpy(dst, src,  sizeof(float)*size_in_floats );  
+    }   
+}
+
+inline void CSGPrimSpec::Dump(std::vector<float>& out)  // static 
+{
+     std::cout << " gather " << out.size() << std::endl ; 
+     for(unsigned i=0 ; i < out.size() ; i++) 
+     {    
+         if(i % 6 == 0) std::cout << std::endl ; 
+         std::cout << std::setw(10) << out[i] << " " ; 
+     } 
+     std::cout << std::endl ; 
+}
+
+
+inline void CSGPrimSpec::dump(const char* msg, int modulo) const 
+{
+    assert( stride_in_bytes % sizeof(float) == 0 ); 
+    unsigned stride_in_floats = stride_in_bytes/sizeof(float) ; 
+    std::cout 
+        << msg 
+        << " num_prim " << num_prim 
+        << " stride_in_bytes " << stride_in_bytes 
+        << " stride_in_floats " << stride_in_floats 
+        << " modulo " << modulo
+        << std::endl 
+        ; 
+
+    for(unsigned i=0 ; i < num_prim ; i++)
+    {   
+        if( modulo == 0 || ( modulo > 0 && i % modulo == 0 ) )
+        {
+            std::cout 
+                << " i " << std::setw(4) << i 
+                << " sbtIndexOffset " << std::setw(4) << *(sbtIndexOffset + i*stride_in_floats)   
+                ; 
+            for(unsigned j=0 ; j < 6 ; j++)  
+                std::cout << std::setw(10) << std::fixed << std::setprecision(3) << *(aabb + i*stride_in_floats + j ) << " "  ;   
+            std::cout << std::endl ; 
+        }
+    }   
+}
+
+
+inline std::string CSGPrimSpec::desc() const 
+{
+    std::stringstream ss ; 
+
+    ss << "CSGPrimSpec"
+       << " primitiveIndexOffset " << std::setw(4) << primitiveIndexOffset
+       << " num_prim " << std::setw(4) << num_prim 
+       << " stride_in_bytes " << std::setw(5) << stride_in_bytes 
+       << " device " << std::setw(2) << device
+       ;
+
+    std::string s = ss.str(); 
+    return s ; 
+}
+
+
+/**
+CSGPrimSpec::downloadDump
+---------------------------
+
+As are starting the read from within the structure, 
+need to trim offsets to avoid reading beyond the array.
+
+**/
+
+inline void CSGPrimSpec::downloadDump(const char* msg) const 
+{
+    assert( device == true ); 
+    unsigned stride_in_values = stride_in_bytes/sizeof(float) ; 
+    unsigned numValues = stride_in_values*num_prim ;   
+    unsigned nff = numValues - CSGPrim__value_offsetof_AABB ;  
+    unsigned nuu = numValues - CSGPrim__value_offsetof_sbtIndexOffset ;
+    std::cout 
+        << "[ " << msg 
+        << " num_prim " << num_prim 
+        << " stride_in_values " << stride_in_values
+        << " numValues " << numValues
+        << " nff " << nff
+        << " nuu " << nuu
+        << " CSGPrim__value_offsetof_AABB " << CSGPrim__value_offsetof_AABB
+        << " CSGPrim__value_offsetof_sbtIndexOffset " << CSGPrim__value_offsetof_sbtIndexOffset
+        << std::endl 
+        ; 
+
+    assert( stride_in_values == 16 ); 
+
+    std::vector<unsigned> uu ; 
+    std::vector<float> ff ; 
+
+    SCU::DownloadVec(ff,           aabb, nff); 
+    SCU::DownloadVec(uu, sbtIndexOffset, nuu ); 
+
+    for(unsigned i=0 ; i < num_prim ; i++)
+    {
+        std::cout << std::setw(5) << *(uu.data()+stride_in_values*i + 0) << " " ;
+        for(int j=0 ; j < 6 ; j++) 
+        {
+            std::cout 
+                << std::fixed << std::setw(8) << std::setprecision(2) 
+                << *(ff.data()+stride_in_values*i + j) 
+                << " " 
+                ;
+        } 
+        std::cout << std::endl ; 
+    }
+    std::cout << "] " << msg << std::endl ; 
+}
+
+
+#endif
+
 
