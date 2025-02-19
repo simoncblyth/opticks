21b65f275958ad04f3b28204fa13d0300637c249
Simon C Blyth <simoncblyth@gmail.com>
2025-02-19 14:27:35 +0800

    update ~/opticks/examples/UseSysRap/go.sh

--- examples/UseSysRap/go.sh
+++ examples/UseSysRap/go.sh
@@ -1,37 +1,28 @@
-#!/bin/bash -l
-##
-## Copyright (c) 2019 Opticks Team. All Rights Reserved.
-##
-## This file is part of Opticks
-## (see https://bitbucket.org/simoncblyth/opticks).
-##
-## Licensed under the Apache License, Version 2.0 (the "License"); 
-## you may not use this file except in compliance with the License.  
-## You may obtain a copy of the License at
-##
-##   http://www.apache.org/licenses/LICENSE-2.0
-##
-## Unless required by applicable law or agreed to in writing, software 
-## distributed under the License is distributed on an "AS IS" BASIS, 
-## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
-## See the License for the specific language governing permissions and 
-## limitations under the License.
-##
+#!/bin/bash 
+usage(){ cat << EOU
+
+~/opticks/examples/UseSysRap/go.sh
+
+EOU
+}
 
 
-opticks-
+cd $(dirname $BASH_SOURCE)
 
 sdir=$(pwd)
 bdir=/tmp/$USER/opticks/$(basename $sdir)/build 
 
 rm -rf $bdir && mkdir -p $bdir && cd $bdir && pwd 
 
+vars="0 BASH_SOURCE sdir bdir OPTICKS_PREFIX"
+for var in $vars ; do printf "%20s : %s\n" "$var" "${!var}" ; done
+
 cmake $sdir \
      -DCMAKE_BUILD_TYPE=Debug \
-     -DCMAKE_INSTALL_PREFIX=$(opticks-prefix) \
-     -DCMAKE_PREFIX_PATH=$(opticks-prefix)/externals \
-     -DCMAKE_MODULE_PATH=$(opticks-home)/cmake/Modules \
-     -DOPTICKS_PREFIX=$(opticks-prefix)
+     -DCMAKE_INSTALL_PREFIX=$OPTICKS_PREFIX \
+     -DCMAKE_PREFIX_PATH=$OPTICKS_PREFIX/externals \
+     -DCMAKE_MODULE_PATH=$sdir/../../cmake/Modules \
+     -DOPTICKS_PREFIX=$OPTICKS_PREFIX
 
 make
 make install   
