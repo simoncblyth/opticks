2b3480476f34daf0e9c61b1d9e57ae49c7f033f2
Simon C Blyth <simoncblyth@gmail.com>
2025-02-19 11:30:02 +0800

    update examples/UsePLog/go.sh to use OPTICKS_PREFIX envvar rather than the bash function

--- examples/UsePLog/go.sh
+++ examples/UsePLog/go.sh
@@ -1,25 +1,4 @@
-#!/bin/bash -l
-##
-## Copyright (c) 2019 Opticks Team. All Rights Reserved.
-##
-## This file is part of Opticks
-## (see https://bitbucket.org/simoncblyth/opticks).
-##
-## Licensed under the Apache License, Version 2.0 (the "License"); 
-## you may not use this file except in compliance with the License.  
-## You may obtain a copy of the License at
-##
-##   http://www.apache.org/licenses/LICENSE-2.0
-##
-## Unless required by applicable law or agreed to in writing, software 
-## distributed under the License is distributed on an "AS IS" BASIS, 
-## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
-## See the License for the specific language governing permissions and 
-## limitations under the License.
-##
-
-opticks-
-#source /opt/rh/devtoolset-8/enable 
+#!/bin/bash
 
 
 sdir=$(pwd)
@@ -33,10 +12,10 @@ pwd
 
 cmake $sdir \
     -DCMAKE_BUILD_TYPE=Debug \
-    -DCMAKE_PREFIX_PATH=$(opticks-prefix)/externals \
-    -DCMAKE_INSTALL_PREFIX=$(opticks-prefix) \
-    -DCMAKE_MODULE_PATH=$(opticks-home)/cmake/Modules \
-    -DOPTICKS_PREFIX=$(opticks-prefix)
+    -DCMAKE_PREFIX_PATH=$OPTICKS_PREFIX/externals \
+    -DCMAKE_INSTALL_PREFIX=$OPTICKS_PREFIX \
+    -DCMAKE_MODULE_PATH=$OPTICKS_PREFIX/cmake/Modules \
+    -DOPTICKS_PREFIX=$OPTICKS_PREFIX
 
 make
 make install   
