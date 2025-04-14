a47d53807d172e56c4957b209495a541aa49f703
Simon C Blyth <simoncblyth@gmail.com>
2025-04-14 16:05:35 +0800

    in g4cx/tests/G4CXTest_raindrop.sh invoke both the python and C++ history comparison

--- g4cx/tests/G4CXTest_raindrop.sh
+++ g4cx/tests/G4CXTest_raindrop.sh
@@ -3,13 +3,13 @@ usage(){ cat << EOU
 G4CXTest_raindrop.sh : Standalone bi-simulation with G4CXApp::Main
 ===================================================================
 
-Certain special GEOM strings such as "RaindropRockAirWater" 
+Certain special GEOM strings such as "RaindropRockAirWater"
 are recognized by U4VolumeMaker::PVS_  which is called by U4VolumeMaker::PV
 
 For the configuratuin of the raindrop see U4VolumeMaker::RaindropRockAirWater_Configure
 
-Currently this uses the default torch genstep for the initial photons, 
-see storch::FillGenstep for how to customize that. 
+Currently this uses the default torch genstep for the initial photons,
+see storch::FillGenstep for how to customize that.
 
 ::
 
@@ -18,41 +18,41 @@ see storch::FillGenstep for how to customize that.
 
     EYE=0,-400,0 ~/opticks/g4cx/tests/G4CXTest_raindrop.sh ana
 
-    PICK=A MODE=3 SELECT="TO BT BR BR BR BR BT SA" ~/opticks/g4cx/tests/G4CXTest_raindrop.sh 
+    PICK=A MODE=3 SELECT="TO BT BR BR BR BR BT SA" ~/opticks/g4cx/tests/G4CXTest_raindrop.sh
     PICK=B MODE=3 SELECT="TO BT BR BR SA"          ~/opticks/g4cx/tests/G4CXTest_raindrop.sh ana
 
-    NUM=1000000 PICK=B MODE=3 SELECT="TO BT BR BR BR BR BT SA" ~/opticks/g4cx/tests/G4CXTest_raindrop.sh 
+    NUM=1000000 PICK=B MODE=3 SELECT="TO BT BR BR BR BR BT SA" ~/opticks/g4cx/tests/G4CXTest_raindrop.sh
 
 EOU
 }
 
 cd $(dirname $(realpath $BASH_SOURCE))
-vars="" 
+vars=""
 DIR=$(pwd)
 bin=G4CXTest
-script=G4CXTest_raindrop.py 
+script=G4CXTest_raindrop.py
 
-export GEOM=RaindropRockAirWater  # GEOM is identifier for a geometry 
+export GEOM=RaindropRockAirWater  # GEOM is identifier for a geometry
 
 vars="$vars BASH_SOURCE PWD DIR bin script GEOM"
 
-# THESE ARE NOW THE DEFAULTS 
+# THESE ARE NOW THE DEFAULTS
 export U4VolumeMaker_RaindropRockAirWater_RINDEX=0,0,1,1.333
 export U4VolumeMaker_RaindropRockAirWater_MATS=VACUUM,G4_Pb,G4_AIR,G4_WATER
 export U4VolumeMaker_RaindropRockAirWater_HALFSIDE=100
-export U4VolumeMaker_RaindropRockAirWater_DROPSHAPE=Box  # default:Orb  (Box also impl) 
+export U4VolumeMaker_RaindropRockAirWater_DROPSHAPE=Box  # default:Orb  (Box also impl)
 
 if [ -n "$KLUDGE" ]; then
-    export U4Recorder__PreUserTrackingAction_Optical_UseGivenVelocity_KLUDGE=1 
+    export U4Recorder__PreUserTrackingAction_Optical_UseGivenVelocity_KLUDGE=1
 fi
 
 if [ "$U4Recorder__PreUserTrackingAction_Optical_UseGivenVelocity_KLUDGE" == "1" ]; then
-    version=1 
+    version=1
 else
-    version=0 
-fi 
+    version=0
+fi
 
-export VERSION=$version  # used in the SEvt output directory 
+export VERSION=$version  # used in the SEvt output directory
 vars="$vars VERSION"
 
 
@@ -62,13 +62,14 @@ export G4CXOpticks__SaveGeometry_DIR=$HOME/.opticks/GEOM/$GEOM
 
 #num=1000
 #num=5000
-num=H1
+#num=H1
+num=M1
 NUM=${NUM:-$num}
 
-## For torch running MUST NOW configure the below two NUM envvars 
+## For torch running MUST NOW configure the below two NUM envvars
 ## with the same number of comma delimited values, or just 1 value
 
-export OPTICKS_NUM_PHOTON=$NUM  
+export OPTICKS_NUM_PHOTON=$NUM
 export OPTICKS_NUM_GENSTEP=1
 export OPTICKS_RUNNING_MODE="SRM_TORCH"
 export OPTICKS_MAX_SLOT=M1
@@ -77,8 +78,8 @@ export OPTICKS_MAX_SLOT=M1
 vars="$vars OPTICKS_NUM_PHOTON OPTICKS_NUM_GENSTEP OPTICKS_RUNNING_MODE"
 
 
-if [ "$OPTICKS_RUNNING_MODE" == "SRM_TORCH" ]; then  
-    #export SEvent_MakeGenstep_num_ph=$NUM   ## NO LONGER USED ? 
+if [ "$OPTICKS_RUNNING_MODE" == "SRM_TORCH" ]; then
+    #export SEvent_MakeGenstep_num_ph=$NUM   ## NO LONGER USED ?
 
     #src="rectangle"
     #src="disc"
@@ -93,7 +94,7 @@ if [ "$OPTICKS_RUNNING_MODE" == "SRM_TORCH" ]; then
         export storch_FillGenstep_type=disc
         export storch_FillGenstep_radius=50        # radius
         export storch_FillGenstep_zenith=0,1       # radial range scale
-        export storch_FillGenstep_azimuth=0,1      # phi segment twopi fraction 
+        export storch_FillGenstep_azimuth=0,1      # phi segment twopi fraction
         export storch_FillGenstep_mom=1,0,0
         export storch_FillGenstep_pos=-80,0,0
     elif [ "$src" == "circle_inwards_hemi" ]; then
@@ -101,12 +102,12 @@ if [ "$OPTICKS_RUNNING_MODE" == "SRM_TORCH" ]; then
         export storch_FillGenstep_radius=-50    # -ve means inwards
         export storch_FillGenstep_pos=0,0,50
         export storch_FillGenstep_azimuth=0.5,1
-    fi 
+    fi
     vars="$vars src"
 fi
 
 
-#oim=2  # CPU only 
+#oim=2  # CPU only
 oim=3  # GPU and CPU optical simulation
 export OPTICKS_INTEGRATION_MODE=${OPTICKS_INTEGRATION_MODE:-$oim}
 
@@ -115,17 +116,21 @@ mode=DebugLite
 export OPTICKS_EVENT_MODE=${OPTICKS_EVENT_MODE:-$mode} # configure what to gather and save
 
 
-# below directories match those used by SEvt saving 
+# below directories match those used by SEvt saving
 # in order to be able to load SEvt into python analysis script
-# 
-export TMP=${TMP:-/tmp/$USER/opticks} 
+#
+export TMP=${TMP:-/tmp/$USER/opticks}
 evtfold=$TMP/GEOM/$GEOM
 
-export AFOLD=$evtfold/$bin/ALL$VERSION/A000
-export BFOLD=$evtfold/$bin/ALL$VERSION/B000
+ctx=$(TEST=ContextString sbuild_test)   ## eg Debug_Philox
+export OPTICKS_EVENT_NAME=${ctx}
+reldir=ALL${VERSION}_${OPTICKS_EVENT_NAME}
+
+export AFOLD=$evtfold/$bin/$reldir/A000
+export BFOLD=$evtfold/$bin/$reldir/B000
 
 
-vars="$vars OPTICKS_INTEGRATION_MODE OPTICKS_EVENT_MODE TMP evtfold AFOLD BFOLD"
+vars="$vars OPTICKS_INTEGRATION_MODE OPTICKS_EVENT_MODE TMP evtfold reldir OPTICKS_EVENT_NAME AFOLD BFOLD"
 
 
 event_debug()
@@ -136,7 +141,7 @@ event_debug()
     export SEvt=INFO
     export SEvent=INFO
 
-    type $FUNCNAME 
+    type $FUNCNAME
 }
 [ -n "$EVENT_DEBUG" ] && event_debug
 
@@ -150,56 +155,74 @@ logging()
 }
 [ -n "$LOG" ] && logging
 
-defarg="info_run_ana"
+defarg="info_run_ana_cf2"
 #defarg="info_dbg_ana"
 arg=${1:-$defarg}
 
-[ -n "$BP" ] && echo $BASH_SOURCE : override arg to info_dbg as BP $BP is defined && arg=info_dbg 
+[ -n "$BP" ] && echo $BASH_SOURCE : override arg to info_dbg as BP $BP is defined && arg=info_dbg
 
-if [ -n "$BP" ]; then 
-   DEBUG_GENIDX=10000 
+if [ -n "$BP" ]; then
+   DEBUG_GENIDX=10000
    export U4VPrimaryGenerator__GeneratePrimaries_From_Photons_DEBUG_GENIDX=$DEBUG_GENIDX
-   # for DEBUG_GENIDX > -1 will only generate one photon : for debugging purposes 
+   # for DEBUG_GENIDX > -1 will only generate one photon : for debugging purposes
    echo $BASH_SOURCE :  DEBUG_GENIDX $DEBUG_GENIDX OPTICKS_NUM_PHOTON $OPTICKS_NUM_PHOTON
-fi 
+fi
 
 
-vars="$vars CUDA_VISIBLE_DEVICES BP defarg arg" 
+vars="$vars CUDA_VISIBLE_DEVICES BP defarg arg"
 
 
 
-if [ "${arg/info}" != "$arg" ]; then 
-    for var in $vars ; do printf "%30s : %s \n" "$var" "${!var}" ; done 
-fi 
+if [ "${arg/info}" != "$arg" ]; then
+    for var in $vars ; do printf "%30s : %s \n" "$var" "${!var}" ; done
+fi
 
 if [ "${arg/run}" != "$arg" ]; then
     $bin
-    [ $? -ne 0 ] && echo $BASH_SOURCE : run error && exit 1 
-fi 
+    [ $? -ne 0 ] && echo $BASH_SOURCE : run error && exit 1
+fi
 
 if [ "${arg/dbg}" != "$arg" ]; then
-    source $DIR/../../bin/dbg__.sh 
-    dbg__ $bin 
-    [ $? -ne 0 ] && echo $BASH_SOURCE : dbg error && exit 2 
-fi 
+    source $DIR/../../bin/dbg__.sh
+    dbg__ $bin
+    [ $? -ne 0 ] && echo $BASH_SOURCE : dbg error && exit 2
+fi
 
 if [ "${arg/grab}" != "$arg" ]; then
     source $DIR/../../bin/rsync.sh $evtfold
-    [ $? -ne 0 ] && echo $BASH_SOURCE : grab error && exit 3 
+    [ $? -ne 0 ] && echo $BASH_SOURCE : grab error && exit 3
+fi
+
+if [ "${arg/old}" != "$arg" ]; then
+    export SAVE_SEL=1
+
+    if command -v ${IPYTHON:-ipython} &> /dev/null
+    then
+        ${IPYTHON:-ipython} --pdb -i $script
+    else
+        echo $BASH_SOURCE - IPYTHON NOT AVAILABLE - TRY PYTHON
+        ${PYTHON:-python} -i $script
+    fi
+    [ $? -ne 0 ] && echo $BASH_SOURCE : ana error with script $script && exit 4
 fi
 
 if [ "${arg/ana}" != "$arg" ]; then
-    export SAVE_SEL=1
-
-    if command -v ${IPYTHON:-ipython} &> /dev/null 
-    then
-        ${IPYTHON:-ipython} --pdb -i $script 
-    else
-        echo $BASH_SOURCE - IPYTHON NOT AVAILABLE - TRY PYTHON 
-        ${PYTHON:-python} -i $script 
-    fi 
+    ${PYTHON:-python} $script
     [ $? -ne 0 ] && echo $BASH_SOURCE : ana error with script $script && exit 4
-fi 
+fi
 
-exit 0 
+if [ "${arg/pdb}" != "$arg" ]; then
+    ${IPYTHON:-ipython} --pdb -i $script
+    [ $? -ne 0 ] && echo $BASH_SOURCE : pdb error with script $script && exit 4
+fi
+
+
+if [ "${arg/cf2}" != "$arg" ]; then
+    $DIR/../../sysrap/tests/sseq_index_test.sh info_run_ana
+    [ $? -ne 0 ] && echo $BASH_SOURCE : cf2 error && exit 5
+fi
+
+
+
+exit 0
 
--- notes/issues/G4CXTest_raindrop_revive_as_demo_of_OPTICKS_INTEGRATION_MODE_3_for_validation.rst
+++ notes/issues/G4CXTest_raindrop_revive_as_demo_of_OPTICKS_INTEGRATION_MODE_3_for_validation.rst
@@ -3,10 +3,10 @@ G4CXTest_raindrop_revive_as_demo_of_OPTICKS_INTEGRATION_MODE_3_for_validation
 
 
 
-Issue 1 : Lack of genstep assert
-----------------------------------
+FIXED : Issue 1 : assert from lack of genstep : due to omitted config
+-----------------------------------------------------------------------
 
-* maybe not updated for SEvt::beginOfEvent SEvt::endOfEvent rejig OR the multi-event index controls 
+* maybe not updated for SEvt::beginOfEvent SEvt::endOfEvent rejig OR the multi-event index controls
 
 ::
 
@@ -17,7 +17,7 @@ Issue 1 : Lack of genstep assert
     Local_DsG4Scintillation::Local_DsG4Scintillation level 0 verboseLevel 0
     2025-04-14 11:01:43.265 FATAL [331468] [U4Physics::CreateBoundaryProcess@412]  FAILED TO SPMTAccessor::Load from [$HOME/.opticks/GEOM/$GEOM/CSGFoundry/SSim/extra/jpmt] GEOM RaindropRockAirWater
     2025-04-14 11:01:43.265 INFO  [331468] [U4Physics::ConstructOp@310]  fBoundary 0x2ce26b0
-    2025-04-14 11:01:43.265 INFO  [331468] [G4CXApp::G4CXApp@160] 
+    2025-04-14 11:01:43.265 INFO  [331468] [G4CXApp::G4CXApp@160]
     U4Recorder__PreUserTrackingAction_Optical_UseGivenVelocity_KLUDGE:0
     $Name: geant4-10-04-patch-02 [MT]$ (25-May-2018)U4Recorder::Switches
     WITH_CUSTOM4
@@ -47,11 +47,11 @@ Issue 1 : Lack of genstep assert
     #10 0x000000000040aa25 in G4CXApp::BeamOn (this=0x6c6400) at /home/blyth/opticks/g4cx/tests/G4CXApp.h:344
     #11 0x000000000040ab31 in G4CXApp::Main () at /home/blyth/opticks/g4cx/tests/G4CXApp.h:351
     #12 0x000000000040acbf in main (argc=1, argv=0x7fffffff4488) at /home/blyth/opticks/g4cx/tests/G4CXTest.cc:13
-    (gdb) 
+    (gdb)
 
     (gdb) f 6
     #6  0x000000000040a1e0 in G4CXApp::GeneratePrimaries (this=0x6c6400, event=0x3349260) at /home/blyth/opticks/g4cx/tests/G4CXApp.h:236
-    236         NP* gs = SEvent::MakeTorchGenstep(idx_arg) ;        
+    236         NP* gs = SEvent::MakeTorchGenstep(idx_arg) ;
     (gdb) p idx_arg
     $1 = 0
     (gdb) f 5
@@ -59,33 +59,33 @@ Issue 1 : Lack of genstep assert
     143 NP* SEvent::MakeTorchGenstep(   int idx_arg){ return MakeGenstep( OpticksGenstep_TORCH, idx_arg ) ; }
     (gdb) f 4
     #4  0x00007ffff3a8e66c in SEvent::MakeGenstep (gentype=6, index_arg=0) at /home/blyth/opticks/sysrap/SEvent.cc:179
-    179     assert( num_gs > 0 ); 
+    179     assert( num_gs > 0 );
     (gdb) p num_gs
     $2 = 0
-    (gdb) 
+    (gdb)
 
 
     (gdb) list
-    174         << " num_ph/M " << num_ph/M 
-    175         << " num_gs " << num_gs 
+    174         << " num_ph/M " << num_ph/M
+    175         << " num_gs " << num_gs
     176         << " dump " << dump
-    177         ; 
-    178 
-    179     assert( num_gs > 0 ); 
-    180 
-    181     NP* gs = NP::Make<float>(num_gs, 6, 4 );  
-    182     gs->set_meta<std::string>("creator", "SEvent::MakeGenstep" ); 
-    183     gs->set_meta<int>("num_ph", num_ph ); 
+    177         ;
+    178
+    179     assert( num_gs > 0 );
+    180
+    181     NP* gs = NP::Make<float>(num_gs, 6, 4 );
+    182     gs->set_meta<std::string>("creator", "SEvent::MakeGenstep" );
+    183     gs->set_meta<int>("num_ph", num_ph );
     (gdb) list 160
-    155 
+    155
     156 **/
-    157 
-    158 
+    157
+    158
     159 NP* SEvent::MakeGenstep( int gentype, int index_arg )
     160 {
-    161     bool with_index = index_arg != -1 ; 
-    162     if(with_index) assert( index_arg >= 0 );  // index_arg is 0-based 
-    163     int num_ph = with_index ? SEventConfig::NumPhoton(index_arg)  : ssys::getenvint("SEvent__MakeGenstep_num_ph", 100 ) ; 
+    161     bool with_index = index_arg != -1 ;
+    162     if(with_index) assert( index_arg >= 0 );  // index_arg is 0-based
+    163     int num_ph = with_index ? SEventConfig::NumPhoton(index_arg)  : ssys::getenvint("SEvent__MakeGenstep_num_ph", 100 ) ;
     164     int num_gs = with_index ? SEventConfig::NumGenstep(index_arg) : ssys::getenvint("SEvent__MakeGenstep_num_gs", 1   ) ;
     (gdb) p with_index
     $1 = true
@@ -95,29 +95,29 @@ Issue 1 : Lack of genstep assert
     $4 = 0
 
 
-Hmm torch running probably needs the comma delimited ph and gs lists 
+Hmm torch running probably needs the comma delimited ph and gs lists
 to have the same numbers of entries
 
 Issue was that OPTICKS_NUM_GENSTEP was not defined, added comment::
 
      65 num=H1
      66 NUM=${NUM:-$num}
-     67 
-     68 ## For torch running MUST NOW configure the below two NUM envvars 
+     67
+     68 ## For torch running MUST NOW configure the below two NUM envvars
      69 ## with the same number of comma delimited values, or just 1 value
-     70 
+     70
      71 export OPTICKS_NUM_PHOTON=$NUM
      72 export OPTICKS_NUM_GENSTEP=1
      73 export OPTICKS_RUNNING_MODE="SRM_TORCH"
-     74 
+     74
      75 vars="$vars OPTICKS_NUM_PHOTON OPTICKS_NUM_GENSTEP OPTICKS_RUNNING_MODE"
 
 
 
 
 
-Issue 2 : VRAM OOM from debug arrays as forgot to set OPTICKS_MAX_SLOT which is needed when debugging
----------------------------------------------------------------------------------------------------------
+FIXED : Issue 2 : VRAM OOM from debug arrays as forgot to set OPTICKS_MAX_SLOT which is needed when debugging
+----------------------------------------------------------------------------------------------------------------
 
 ::
 
@@ -126,7 +126,7 @@ Issue 2 : VRAM OOM from debug arrays as forgot to set OPTICKS_MAX_SLOT which is 
 
     2025-04-14 11:38:45.145 INFO  [410693] [G4CXApp::BeamOn@343] [ OPTICKS_NUM_EVENT=1
     2025-04-14 11:38:45.529 INFO  [410693] [G4CXApp::GeneratePrimaries@223] [ SEventConfig::RunningModeLabel SRM_TORCH eventID 0
-    SGenerate::GeneratePhotons SGenerate__GeneratePhotons_RNG_PRECOOKED : NO 
+    SGenerate::GeneratePhotons SGenerate__GeneratePhotons_RNG_PRECOOKED : NO
     U4VPrimaryGenerator::GeneratePrimaries_From_Photons ph (100000, 4, 4, )
      U4VPrimaryGenerator__GeneratePrimaries_From_Photons_DEBUG_GENIDX : -1 (when +ve, only generate tht photon idx)
     2025-04-14 11:38:45.601 INFO  [410693] [G4CXApp::GeneratePrimaries@253] ]  eventID 0
@@ -161,7 +161,7 @@ Issue 2 : VRAM OOM from debug arrays as forgot to set OPTICKS_MAX_SLOT which is 
     ]salloc.meta
 
          [           size   num_items sizeof_item       spare]    size_GB    percent label
-         [        (bytes)                                    ]   size/1e9            
+         [        (bytes)                                    ]   size/1e9
 
          [              8           1           8           0]       0.00       0.00 QBase::init/d_base
          [             24           1          24           0]       0.00       0.00 QRng::initMeta/d_qr
@@ -184,15 +184,35 @@ Issue 2 : VRAM OOM from debug arrays as forgot to set OPTICKS_MAX_SLOT which is 
     ==========================
 
     When running with debug arrays, such as the record array, enabled
-    it is necessary to set max_slot to something reasonable, otherwise with the 
-    default max_slot of zero, it gets set to a high value (eg M197 with 24GB) 
-    appropriate for production running with the available VRAM. 
+    it is necessary to set max_slot to something reasonable, otherwise with the
+    default max_slot of zero, it gets set to a high value (eg M197 with 24GB)
+    appropriate for production running with the available VRAM.
 
-    One million is typically reasonable for debugging:: 
+    One million is typically reasonable for debugging::
 
        export OPTICKS_MAX_SLOT=M1
 
 
 
+
+FIXED : Issue 3 : ana python was not loading the just created SEvt due to outdated AFOLD BFOLD
+----------------------------------------------------------------------------------------------------
+
+* observed from the sevt.py array ages logging
+* fixed with OPTICKS_EVENT_NAME
+
+
+FIXED : Issue 4 : not that SEvt chi2 comparison is using the old slow python not the C++ equivalent
+------------------------------------------------------------------------------------------------------
+
+Is this it ?::
+
+    P[blyth@localhost tests]$ l sseq_index_test.*
+    4 -rwxrwxr-x. 1 blyth blyth 2143 Apr 14 14:42 sseq_index_test.sh
+    4 -rw-rw-r--. 1 blyth blyth 1622 Nov 27 15:19 sseq_index_test.cc
+    4 -rw-rw-r--. 1 blyth blyth 1082 May 22  2024 sseq_index_test.py
+    P[blyth@localhost tests]$
+
+Added cf2 command using sseq_index_test.sh
 
 
