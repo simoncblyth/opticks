6385021a1de30d6b4e7202af9abab4d6d3e31568
Simon C Blyth <simoncblyth@gmail.com>
2025-05-15 11:29:12 +0800

    prepare to tag v0.4.2

--- okconf/OpticksVersionNumber.hh
+++ okconf/OpticksVersionNumber.hh
@@ -4,16 +4,16 @@
  * This file is part of Opticks
  * (see https://bitbucket.org/simoncblyth/opticks).
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License.  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
- * See the License for the specific language governing permissions and 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
@@ -29,13 +29,13 @@ Definition of version integer
 **/
 
 
-#define OPTICKS_VERSION_NUMBER 41
+#define OPTICKS_VERSION_NUMBER 42
 
 
 #include "OKCONF_API_EXPORT.hh"
-struct OKCONF_API OpticksVersionNumber 
+struct OKCONF_API OpticksVersionNumber
 {
-    static unsigned Integer(); 
+    static unsigned Integer();
 };
 
 
