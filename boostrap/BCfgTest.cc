22522e176409e60e40d5d759f14b434e0628f857
Simon C Blyth <simoncblyth@gmail.com>
2026-04-09 17:17:19 +0800

    remove the ancient boostrap

--- boostrap/BCfgTest.cc
+++
@@ -1,9 +0,0 @@
-
-#include <iostream>
-#include "BCfg.hh"
-
-int main(int argc, char** argv)
-{
-    std::cout << argv[0] << std::endl ; 
-    return 0 ; 
-}
--- boostrap/tests/BOpticksResourceTest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l 
-
-export GEOM=J004 
-
-source $(dirname $BASH_SOURCE)/../../bin/GEOM_.sh 
-
-BOpticksResourceTest
-
-
